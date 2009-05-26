3a60ecd3de4de762948068c07ba723500b09c739
Luiz Magno <magno@vaio.(none)>
2009-05-26 16:40:57 -0300

    teste para desconto de prazo medio

--- features/step_definitions/desconto_prazo_step.rb.rb
+++ features/step_definitions/desconto_prazo_step.rb.rb
@@ -1,41 +1,12 @@
-Dado /^que eu estou no ItemPedido$/ do
-  @pedido = Pedido.find(:first)
-  @itens = Pedido.itens.find(1)
-end
-
-Dado /^que o valor_base é de 15000$/ do
-  @valor_base = 15000
-end
-
-Dado /^que o prazo medio do pedido é de 200$/ do
-  prazo_medio = 200
-end
-
-Dado /^que o parametro do sistema é 75$/ do
-  @prazo_param = 75
-end
-
-Entao /^ao salvar o item do pedido$/ do
-  itempedido.save
-end
-
-Entao /^eu preciso aplicar o desconto_no_prazo encontrado ao valor_base para comissão$/ do
-  pending
-end
-
-Entao /^eu preciso obter a mensagem: "([^\"]*)"$/ do |arg1|
-  puts arg1
-end
-
-
 Dado /^eu pedir o Calculo da Comissao por Prazo$/ do
-  pending
+  @pedido = Pedido.find(:first)
+  @pedido.desconto_comissao_prazo!
 end
 
 Quando /^eu salvar o pedido$/ do
-  pending
+  @pedido.save
 end
 
 Então /^o valor da comissao calculada deve ser 3$/ do
-  pending
+  @pedido.comissao_vendedor = 3
 end
