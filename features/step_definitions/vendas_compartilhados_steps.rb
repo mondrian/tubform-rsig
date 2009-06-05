2b47d5a6ea6e9087031880c13c5037db074e1086
Jefferson Jean Martins Girao <contato@jefferson.eti.br>
2009-06-05 18:10:51 -0300

    Alteracoes na autenticacao

--- features/step_definitions/vendas_compartilhados_steps.rb
+++ features/step_definitions/vendas_compartilhados_steps.rb
@@ -4,9 +4,11 @@ Dado /^que estou no formulário de cadastro de (.*)$/ do |entidade|
 end
 
 Quando /^defino (.*) com o valor (.*)$/ do |atributo,valor|
- atributo = atributo.gsub(/\s/, '_')
- atributo = @entidade_principal + "_" + atributo
- fill_in atributo, :with => valor
+  atributo = atributo.gsub(/\s/, '_')
+  if ((atributo != "login") and (atributo != "password")) then
+    atributo = @entidade_principal + "_" + atributo
+  end
+  fill_in atributo, :with => valor
 end
 
 Quando /^seleciono (.*) com o valor (.*)$/ do |atributo,valor|
@@ -96,7 +98,7 @@ end
 
 
 Quando /^eu pedir autenticação$/ do
-  click_button("Autenticar")
+  click_button("Entrar")
 end
 
 Dado /^que me identifiquei como (.+) com a senha (.+)$/ do |login, senha|
@@ -110,8 +112,8 @@ Dado /^que me identifiquei como (.+) com a senha (.+)$/ do |login, senha|
     | pedidos         | desconto    |
   })
   Dado "que estou em autenticação"
-  Dado "defino usuário com o valor #{login}"
-  Dado "defino senha com o valor #{senha}"
+  Dado "defino login com o valor #{login}"
+  Dado "defino password com o valor #{senha}"
   Quando "eu pedir autenticação"
 end
 
