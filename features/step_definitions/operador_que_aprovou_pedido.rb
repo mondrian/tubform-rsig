bc66cd6fe3368ce3bc747317bd0b28490a4f8179
Jefferson Jean Martins Girão <jefferson@jefferson-desktop.(none)>
2009-06-22 13:35:32 -0300

    Limpados passos desnecesários

--- features/step_definitions/operador_que_aprovou_pedido.rb
+++ features/step_definitions/operador_que_aprovou_pedido.rb
@@ -1,6 +1,1 @@
-Quando /^eu clicar no botão Aprovar$/ do
-  click_button "Desaprovar Pedido"
-  sleep 2
-  click_button "Aprovar"
-  sleep 2
-end
+
--- features/step_definitions/verificar_permissao_usuario_dar_desconto_Item.rb
+++ features/step_definitions/verificar_permissao_usuario_dar_desconto_Item.rb
@@ -1,12 +1,1 @@
-Quando /^eu clicar no botão dar desconto$/ do
-	click_button "Desconto"
-end
 
-Então /^verifica se tenho permissão$/ do
-  response.should_not contain("Acesso negado a pedidos acao permitir_desconto_no_pedido. Você não tem acesso a esta ação.")
-end
-
-Então /^me redireciona para a pagina de desconto$/ do
-	click_button "Salvar"
-	sleep 6
-end
