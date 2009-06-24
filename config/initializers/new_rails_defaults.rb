7d731f97717d4dd8a7464e9b3f2c42e1848224e3
cmilfont <cmilfont@cmilfont.(none)>
2009-06-24 14:28:01 -0300

    incluindo modelo de layout

--- config/initializers/new_rails_defaults.rb
+++ config/initializers/new_rails_defaults.rb
@@ -5,7 +5,7 @@
 
 if defined?(ActiveRecord)
   # Include Active Record class name as root for JSON serialized output.
-  ActiveRecord::Base.include_root_in_json = true
+  ActiveRecord::Base.include_root_in_json = false
 
   # Store the full class name (including module namespace) in STI type column.
   ActiveRecord::Base.store_full_sti_class = true
@@ -17,3 +17,4 @@ ActiveSupport.use_standard_json_time_format = true
 # Don't escape HTML entities in JSON, leave that for the #json_escape helper.
 # if you're including raw json in an HTML page.
 ActiveSupport.escape_html_entities_in_json = false
+
