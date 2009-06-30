644abc5067453d969f3127452ca292fa7da2ce7d
Luiz Magno <magno@pc5-desktop.(none)>
2009-06-30 17:50:38 -0300

    nota fiscal + impressao

--- app/models/nota_fiscal.rb
+++ app/models/nota_fiscal.rb
@@ -16,4 +16,39 @@ class NotaFiscal < ActiveRecord::Base
   validates_presence_of :qtde_volumes,    :message => "Informe Quantidade de Volumes" 
     
   validates_numericality_of   :percentual_icms
+
+  def atualiza_total_mercadoria!
+     soma = 0
+     for item in self.itens do 
+       soma += item.quantidade * item.valor_item
+     end
+   self.total_mercadoria = soma
+   self.save
+   soma
+  end
+
+  def atualiza_total_nota!
+    soma = 0
+    ipi = 0
+    for item in self.itens do 
+      soma += (item.quantidade * item.valor_item) 
+      ipi +=  (((item.quantidade * item.valor_item) * item.perc_ipi).to_f/100)
+    end
+    self.total_nota = soma + ipi + self.valor_frete
+    self.valor_ipi = ipi
+    self.save
+    soma + ipi
+  end 
+
+  def atualiza_total_icms!
+    soma = 0
+    icms = 0
+    for item in self.itens do 
+      soma += (item.quantidade * item.valor_item) if item.perc_icms != 0
+      icms +=  (((item.quantidade * item.valor_item) * item.perc_icms).to_f/100) if item.perc_icms != 0
+    end
+    self.base_calculo_icms = soma
+    self.valor_icms = icms
+    self.save
+  end
 end
