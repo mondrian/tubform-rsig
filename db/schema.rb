a4869a61d03169087b08cee611571aedd8dd4c2b
Luiz Magno <magno@vaio.(none)>
2009-07-14 15:12:48 -0300

    resolucao de conflito

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-#   This file is auto-generated from the current state of the database. Instead of editing this file, 
+# This file is auto-generated from the current state of the database. Instead of editing this file, 
 # please use the migrations feature of Active Record to incrementally modify your database, and
 # then regenerate this schema definition.
 #
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090709193456) do
+ActiveRecord::Schema.define(:version => 20090713192408) do
 
   create_table "acoes", :force => true do |t|
     t.string   "controller_name"
@@ -545,6 +545,13 @@ ActiveRecord::Schema.define(:version => 20090709193456) do
     t.string   "descricao"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "replicacoes_pendentes", :force => true do |t|
+    t.text     "sql"
+    t.integer  "status",       :limit => 2
+    t.datetime "temporizador"
+    t.integer  "contador"
   end
 
   create_table "roteiros", :force => true do |t|
