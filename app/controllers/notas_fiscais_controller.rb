d53084157a43f9beb2a121644d25437f2a47bf33
Luciano Bezerra <luciano@luciano-laptop.(none)>
2009-07-15 08:18:06 -0300

    Ajustada Nota Fiscal ao Layout anterior (sem ext)

--- app/controllers/notas_fiscais_controller.rb
+++ app/controllers/notas_fiscais_controller.rb
@@ -1,40 +1,12 @@
 class NotasFiscaisController < ApplicationController
-	# bem no metadata_for vai os campos que são symbol do @coisas dentro da interação do for, para serem renderizados e retornados ao cliente, por exemplo id, valor, vendedor e cliente_nome.
-  controller_crud_methods_for NotaFiscal, :metadata_for => ['numero_nota', 'selo', 'serie', 'nome_cliente', 'percentual_icms', 'valor_icms', 'status']
-
-def index
-    @notas_fiscais = NotaFiscal.find(:all, :conditions => @conditions, :joins => :cliente).paginate( :page => @page,
-                                                                  :per_page => @per_page )
-    @coisas = []
-		#pedidos retorna um array de hash, então, faz-se um laço em pedido pegando apenas os campos que quer retorna para o lado client-side, para não ficar pesado demais.
-    for p in @notas_fiscais
-			@coisas << {:id => p.id, :numero_nota => p.numero_nota, :selo => p.selo, :serie => p.serie, :nome_cliente => p.cliente.razao_social, :percentual_icms => p.percentual_icms, :valor_icms => p.valor_icms, :status => p.status }
-    end
-
-    render_from_hash(@coisas)
-
-  end
-
-=begin
-  <td align="center"><%=h n.numero_nota %></td>
-  <td align="center"><%=h n.selo %></td>
-  <td align="center"><%=h n.serie %></td>
-  <td align="left"><%=h n.cliente.razao_social %></td>
-  <td align="right"><%=h n.total_nota.real.real_formatado %></td>
-  <td align="right"><%=h n.percentual_icms.to_s + ' %' %></td>
-  <td align="right"><%=h n.valor_icms.real.real_formatado %></td>
-  <td align="center"><%=h n.status == 'S' ? 'Sim' : 'Não' %></td>
-
   # GET /notas_fiscais
   # GET /notas_fiscais.xml
-
-  layout 'temp'
   def index
     @notas_fiscais = NotaFiscal.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @notas_fiscais }
+# =>       format.xml  { render :xml => @notas_fiscais }
     end
   end
 
@@ -45,7 +17,7 @@ def index
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @nota_fiscal }
+#      format.xml  { render :xml => @nota_fiscal }
     end
   end
 
@@ -56,7 +28,7 @@ def index
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @nota_fiscal }
+#      format.xml  { render :xml => @nota_fiscal }
     end
   end
 
@@ -72,9 +44,9 @@ def index
 
     respond_to do |format|
       if @nota_fiscal.save
-        flash[:notice] = 'Nota Fiscal Gravada, Insira os Itens ...'
+        flash[:notice] = 'NotaFiscal was successfully created.'
         format.html { redirect_to(@nota_fiscal) }
-        format.xml  { render :xml => @nota_fiscal, :status => :created, :location => @nota_fiscal }
+ #       format.xml  { render :xml => @nota_fiscal, :status => :created, :location => @nota_fiscal }
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @nota_fiscal.errors, :status => :unprocessable_entity }
@@ -89,12 +61,12 @@ def index
 
     respond_to do |format|
       if @nota_fiscal.update_attributes(params[:nota_fiscal])
-        flash[:notice] = 'Nota Fiscal Atualizada ....'
+        flash[:notice] = 'NotaFiscal was successfully updated.'
         format.html { redirect_to(@nota_fiscal) }
-        format.xml  { head :ok }
+#        format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @nota_fiscal.errors, :status => :unprocessable_entity }
+#        format.xml  { render :xml => @nota_fiscal.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -107,7 +79,7 @@ def index
 
     respond_to do |format|
       format.html { redirect_to(notas_fiscais_url) }
-      format.xml  { head :ok }
+#      format.xml  { head :ok }
     end
   end
 
@@ -121,5 +93,6 @@ def index
     end
 		redirect_to(:action => "index")
 	end
-=end
+
 end
+
